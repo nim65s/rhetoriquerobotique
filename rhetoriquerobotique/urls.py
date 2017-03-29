49b068f9e06d58e4ec2042a14c64dec4bd36b0c6
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-03-29 15:07:49 +0200

    trucs en fonction de la page actuelle

--- rhetoriquerobotique/urls.py
+++ rhetoriquerobotique/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from .views import InscriptionView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', InscriptionView.as_view(), name='inscription'),
-    url(r'^description', TemplateView.as_view(template_name='description.html'), name='description'),
+    url(r'^$', RedirectView.as_view(pattern_name='inscription', permanent=True), name='home'),
+    url(r'^inscription$', InscriptionView.as_view(), name='inscription'),
+    url(r'^presentation', TemplateView.as_view(template_name='presentation.html'), name='presentation'),
     url(r'^programme', TemplateView.as_view(template_name='programme.html'), name='programme'),
 ]
